f9f2af73ecebe3b4fe34c7efc7a981423803ed55
Nikolai Lønberg <niklonberg@gmail.com>
2023-10-18 13:23:35 +0200

    add toggle theme button, add app.js file and reference to button, toggling dark class on document element on click

---
+++ app.js
@@ -0,0 +1,7 @@
+const log = console.log;
+
+const toggleTheme = document.querySelector("#toggle-theme");
+
+toggleTheme.addEventListener("click", () => {
+  document.documentElement.classList.toggle("dark");
+});
