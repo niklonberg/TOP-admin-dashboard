f3221cff8a043b3725d601b3562e4a9e6f0a7438
Nikolai Lønberg <niklonberg@gmail.com>
2023-10-18 16:02:48 +0200

    begin implementing localStorage userTheme preference

--- app.js
+++ app.js
@@ -2,12 +2,29 @@ const log = console.log;
 
 const toggleTheme = document.querySelector("#toggle-theme");
 
-toggleTheme.addEventListener("click", () => {
-  document.documentElement.classList.toggle("dark-theme");
-});
+let userSetThemePreference = localStorage.getItem("userSetDarkThemeOn");
+log(userSetThemePreference);
 
 const prefersDark = window.matchMedia("(prefers-color-scheme:dark)").matches;
 
-if (prefersDark) {
+/* if (userSetThemePreference === true) {
   document.documentElement.classList.add("dark-theme");
-}
+} else if (!userSetThemePreference) {
+  document.documentElement.classList.remove("dark-theme");
+} else if (prefersDark) {
+  document.documentElement.classList.add("dark-theme");
+} */
+
+toggleTheme.addEventListener("click", () => {
+  if (document.documentElement.classList.contains("dark-theme")) {
+    localStorage.setItem("userSetDarkThemeOn", false);
+  } else {
+    localStorage.setItem("userSetDarkThemeOn", true);
+  }
+
+  document.documentElement.classList.toggle("dark-theme");
+  userSetThemePreference = localStorage.getItem("userSetDarkThemeOn");
+
+  log("user set theme:", userSetThemePreference);
+  log("user prefers dark", prefersDark);
+});
