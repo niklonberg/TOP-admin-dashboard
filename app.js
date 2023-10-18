a06b46ca0f45c10f51d03bd7e59a3cd5e1549d3a
Nikolai Lønberg <niklonberg@gmail.com>
2023-10-18 14:56:59 +0200

    implement adding of dark theme class according to user preference

--- app.js
+++ app.js
@@ -5,3 +5,9 @@ const toggleTheme = document.querySelector("#toggle-theme");
 toggleTheme.addEventListener("click", () => {
   document.documentElement.classList.toggle("dark-theme");
 });
+
+const prefersDark = window.matchMedia("(prefers-color-scheme:dark)").matches;
+
+if (prefersDark) {
+  document.documentElement.classList.add("dark-theme");
+}
